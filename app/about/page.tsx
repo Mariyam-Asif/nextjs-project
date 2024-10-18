bc413fd847b1122c2c6a96a8738a164d67add2f8
Mariyam-Asif <mariyamasif530@gmail.com>
2024-10-18 18:38:14 +0500

    Added missing files

---
+++ app/about/page.tsx
@@ -0,0 +1,45 @@
+export default function About() {
+  return (
+    <section className="container mx-auto py-16 px-6 max-w-4xl bg-white border border-gray-200 shadow-lg rounded-lg">
+      <h2 className="text-4xl font-semibold text-gray-900 mb-4 text-center">
+        About
+      </h2>
+      <div className="mb-10">
+        <h3 className="text-2xl font-medium text-blue-500 mb-3 text-center">
+          Hi, I'm Mariyam
+        </h3>
+        <p className="text-xl text-center text-gray-700">
+          A Full-Stack Developer based in Pakistan.
+        </p>
+      </div>
+     
+      <div className="mb-8">
+        <h4 className="text-xl font-semibold text-gray-800 mb-3">My Passion</h4>
+        <p className="text-lg text-gray-600">
+          I am passionate about delivering exceptional web experiences that seamlessly blend modern design with innovative technology.
+        </p>
+      </div>
+
+      <div className="mb-8">
+        <h4 className="text-xl font-semibold text-gray-800 mb-3">Commitment to Quality</h4>
+        <p className="text-lg text-gray-600">
+          I am dedicated to creating solutions that not only meet the needs of users but also exceed expectations.
+        </p>
+      </div>
+
+      <div className="mb-8">
+        <h4 className="text-xl font-semibold text-gray-800 mb-3">Collaboration</h4>
+        <p className="text-lg text-gray-600">
+          I emphasize client collaboration, ensuring open and effective communication throughout every project.
+        </p>
+      </div>
+
+      <div className="mb-8">
+        <h4 className="text-xl font-semibold text-gray-800 mb-3">Creativity Meets Technology</h4>
+        <p className="text-lg text-gray-600">
+          Blending creativity with technical expertise, I aim to transform your vision into standout solutions that make a real impact.
+        </p>
+      </div>
+    </section>
+  );
+}
